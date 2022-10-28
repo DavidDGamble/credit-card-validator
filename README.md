1712c57e7745aadd88a1a3e1f120aa22d7093ca1
DavidDGamble <daviddgamble@gmail.com>
2022-10-27 20:48:53 -0700

    Add more js business logic, user interface logic, and html

--- README.md
+++ README.md
@@ -4,10 +4,10 @@ Test: 'It should check lenght of card number for #>15 #<17'
 Code: cardNumberLength(4102080860435620);
 Expected Output: True
 
-Describe: cardNumberCovert()
+Describe: cardNumberTotal()
 
-Test: 'It take the card number and find its total'
-Code: cardNumberConvert('4204070780465320')
+Test: 'It takes the card number and finds it's total'
+Code: numberTotal('4204070780465320')
 Expected Output: 52
 
 Describe: cardTotalValid()
@@ -17,4 +17,15 @@ Code: cardTotalValid(49);
 Expected output:
 True
 
-Describe:
+Describe:cardConvert()
+
+Test: 'It should run card number through the Luhn Algorithm.'
+Code: cardConvert('4102080880435620')
+Expected Outcome: '4204070780465320'
+
+Describe: cardDefine()
+
+Test: 'It should define what type of credit card the number belongs to.'
+Code: cardDefine('3402080860435620')
+Expected Outcome:
+'American Express'
--- js/scripts.js
+++ js/scripts.js
@@ -39,29 +39,34 @@ function cardConvert(cardNumber) {
   return cardArray.join('');
 }
 
+function cardDefine(cardNumber) {
+  const cardArray = cardNumber.split('');
+  if (parseInt(cardArray[0]) === 3 && parseInt(cardArray[1]) === 4 || parseInt(cardArray[0]) === 3 && parseInt(cardArray[1]) === 7) {
+    return 'American Express';
+  } else if (parseInt(cardArray[0]) === 4) {
+    return 'Visa';
+  } else if (parseInt(cardArray[0]) === 5) {
+    return 'Mastercard';
+  } else if (parseInt(cardArray[0]) === 6) {
+    return 'Discover Card'
+  }
+}
 
-console.log(cardConvert('4102080880435620'));
+window.addEventListener("load" , function(){
+  let form = document.querySelector("form");
+  form.addEventListener("submit" , handleSubmit);
+});
 
-
-// function cardNumberConvert(cardNumber) {
-//   let isValid = false;
-//   const cardArray = cardNumber.split('');
-//   for (let i = 1; i <= cardArray.length; i += 2) {
-//     let currentNum = parseInt(cardArray[i]) * 2;
-//     if (currentNum > 9) {
-//       let currentNumArray = currentNum.toString().split('');
-//       currentNum = parseInt(currentNumArray[0]) + parseInt(currentNumArray[1]);
-//     }
-//       cardArray[i] = currentNum.toString();
-//   }
-//   let total = 0;
-//   cardArray.forEach(function(element) {
-//     total += parseInt(element);
-//   });
-//   const totalArray = total.toString().split('');
-//   if (parseInt(totalArray[totalArray.length]) !== 0) {
-//     isValid = true;
-//   }
-//   return isValid;
-// }
-
+function handleSubmit(event) {
+  event.preventDefault();
+  const cardNumberInput = document.getElementById('cardNumber').value;
+  if (cardNumberLength(cardNumberInput)) {
+    if (cardTotalValid(numberTotal(cardConvert(cardNumberInput))) === false) {
+      document.getElementById('output').innerText = 'INVALID CARD NUMBER!'; 
+    } else {
+      document.getElementById('output').innerText = 'Valid ' + cardDefine(cardNumberInput);
+    }
+  } else {
+    document.getElementById('output').innerText = 'INVALID CARD LENGTH!';
+  }
+}
